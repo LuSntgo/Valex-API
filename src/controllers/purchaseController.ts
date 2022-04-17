14a164968e3dc0c8572b9c6234f8dec5a03e51e8
Luiza Santiago <lu.sntgo@gmail.com>
2022-04-17 17:51:05 -0300

    feature purchase card

--- src/controllers/purchaseController.ts
+++ src/controllers/purchaseController.ts
@@ -2,5 +2,15 @@ import { Request, Response } from "express";
 import * as cardService from "../services/cardServices.js";
 
 export async function purchaseCard(req: Request, res: Response) {
+  const { cardId, businessId } = req.params;
+  const { password, amount } = req.body;
 
+  await cardService.purchaseCard(
+    parseInt(cardId),
+    parseInt(businessId),
+    password,
+    amount
+  );
+
+  res.sendStatus(200);
 }
