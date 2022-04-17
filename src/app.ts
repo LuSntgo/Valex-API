ee300e4bbeccef6669591a30b488a1144564e3dd
Luiza Santiago <lu.sntgo@gmail.com>
2022-04-17 18:01:11 -0300

    fix error

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(router);
 
 export default app;
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}.`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}.`);
 });
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import cardRouter from "./cardRouter.js";
+import purchaseRouter from "./purchaseRouter.js";
 import rechargeRouter from "./rechargeRouter.js";
 const router = Router();
 
 router.use(cardRouter)
 router.use(rechargeRouter)
+router.use(purchaseRouter)
 
 export default router;
